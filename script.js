5ac7174a959b6ea5f8f1ad1a0a0d5c9dadca723b
Andre <andreluizvendrame@gmail.com>
2026-08-13 16:31:15 -0300

    Alterações no Css e mínimo de números de crinaças e adultos no html

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-const select = document.querySelector ("select")
-const visor = document.querySelector ("#visor")
-const inputs = document.querySelectorAll ("input")
+const select = document.querySelector("select")
+const visor = document.querySelector("#visor")
+const inputs = document.querySelectorAll("input")
 const ps = []
 
 
-for (index in inputs){
+for (index in inputs) {
 
-    let inpuuut = inputs [index]
+    let inpuuut = inputs[index]
 
-    inpuuut.addEventListener ("change" , function (cleide){
-        
+    inpuuut.addEventListener("change", function (cleide) {
+
         cleide.preventDefault
-        console.log (inpuuut)
-        
-        if (ps.length >= inpuuut && ps [index] === inpuuut.value){
-            alert ("Bullshit")
+        console.log(inpuuut)
+
+        if (ps.length >= inpuuut && ps[index] === inpuuut.value) {
+            alert("Bullshit")
         }
     })
 }
